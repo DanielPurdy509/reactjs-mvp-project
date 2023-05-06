c49e1eab2d82570978a542b14b051921a8c9d53d
DanielPurdy509 <danielpurdy509@gmail.com>
2023-05-06 16:51:04 -0700

    Seed and Migration files created for DB

---
+++ db/migration.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS photos;
+
+CREATE TABLE photos (
+    id serial PRIMARY KEY,
+    image_src varchar(50),
+    description text,
+    photo_date date
+);
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO photos (image_src,description,photo_date) VALUES
+('src/assets/DSC_1052','Tolmie Peak, WA','2022-09-08'),
+('src/assets/DSC_0637','Cape Flatery, WA','2022-07-16'),
+('src/assets/DSC_0084','Haedong Yonggungsa Temple, KR','2019-05-30');
