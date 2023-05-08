dda9447e0cbf7e3eeaf6e633e9198a0c7601971a
DanielPurdy509 <danielpurdy509@gmail.com>
2023-05-08 02:16:24 -0700

    Issues with React launching and server crashing fixed

---
+++ server/express.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import pg from "pg";
+const { Client } = pg;
+
+const app = express();
+app.use(express.static("public"));  //<-- right after `app` is created and before routes
+
+dotenv.config();  //<-- has to be before 'process.env' is called
+const port = process.env.PORT || 3000;
+
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
+
+app.use(express.json());  //<--has to be before routes
+
+app.get("/", function(req, res) {   
+    res.send("Hello, world!");
+});
+
+app.get('/api', function(req, res) {
+    client.query(`SELECT * FROM photos`, function(err, response) {
+        console.log(err ? err : response.rows)
+        res.json(response.rows)
+    })
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`Server started on port ${port}`);
+    }
+});
