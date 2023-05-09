249b5b21e629b82507af114b0335bbf649f2df14
DanielPurdy509 <danielpurdy509@gmail.com>
2023-05-09 12:55:33 -0700

    Readme created

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# ReactJS - MVP Project
+
+
+
+A MVP website built over the course of two days.
+
+Modeled after a photo portfolio website for a photographer using photos provided by me.
+
+Includes seperate "page" displays for a home/landing page, about page, and the gallery page. All navigation is done through a fixed nav bar.
+
+All photo information, including image sources, image descriptions, dates, and photo titles are stored through a single-table database.
+
+Data is accessed from the database via an Express.js server. Currently there is only routing for retrieving all the database files.
+
+Project was not deployed
+
+
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <div className="word_block">
                 <h2>This page is a Work in Progress</h2>
                 <p>
-                    I wanted to create a space to store my photography from my journies while also practicing my web development skills. This webpage was created using React for the Front End and Express for the Back End server.
+                    I wanted to create a space to store my photography from my journeys while also practicing my web development skills. This webpage was created using React for the Front End and Express for the Back End server.
                 </p>
                 <p>
                     This is intentended to be an ongoing project. More features and functionality will be incorporated over time. Feel free to let me know what you think and where I can improve!
